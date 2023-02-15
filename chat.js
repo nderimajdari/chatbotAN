3af9edb7c2ef53c9cd2eac1d2a2ff5989b2fafd8
nderimajdari <123307571+nderimajdari@users.noreply.github.com>
2023-02-15 23:45:45 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,97 +1,104 @@
 var coll = document.getElementsByClassName("collapsible");
 
 for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function () {
-        this.classList.toggle("active");
+  coll[i].addEventListener("click", function () {
+    this.classList.toggle("active");
 
-        var content = this.nextElementSibling;
+    var content = this.nextElementSibling;
 
-        if (content.style.maxHeight) {
-            content.style.maxHeight = null;
-        } else {
-            content.style.maxHeight = content.scrollHeight + "px";
-        }
-
-    });
+    if (content.style.maxHeight) {
+      content.style.maxHeight = null;
+    } else {
+      content.style.maxHeight = content.scrollHeight + "px";
+    }
+  });
 }
 
 function getTime() {
-    let today = new Date();
-    hours = today.getHours();
-    minutes = today.getMinutes();
+  let today = new Date();
+  hours = today.getHours();
+  minutes = today.getMinutes();
 
-    if (hours < 10) {
-        hours = "0" + hours;
-    }
+  if (hours < 10) {
+    hours = "0" + hours;
+  }
 
-    if (minutes < 10) {
-        minutes = "0" + minutes;
-    }
+  if (minutes < 10) {
+    minutes = "0" + minutes;
+  }
 
-    let time = hours + ":" + minutes;
-    return time;
+  let time = hours + ":" + minutes;
+  return time;
 }
 
-function firstBotMessage() {
-    let firstMessage = "Hello, I'm chatbot AN! How can I help you today?"
-    document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+let firstMessage = "Hello, I'm chatbot AN! How can I help you today?"
+document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
 
-    let time = getTime();
-
-    $("#chat-timestamp").append(time);
-    document.getElementById("userInput").scrollIntoView(false);
-}
-
-firstBotMessage();
+let time = getTime();
+$("#chat-timestamp").append(time);
+document.getElementById("userInput").scrollIntoView(false);
 
 function getHardResponse(userText) {
     let botResponse = getBotResponse(userText);
-    let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
-    $("#chatbox").append(botHtml);
-
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
-}
+    let botHtml = '<p class="botText"><span></span></p>';
+  
+    // append bot's response after the last user message
+    $(".userText:last").after(botHtml);
+  
+    let i = 0;
+    let intervalId = setInterval(function () {
+      if (i <= botResponse.length) {
+        $(".botText:last span").text(botResponse.slice(0, i));
+        i++;
+      } else {
+        clearInterval(intervalId);
+        let botHtml = '<p class="botText"><span>' + botResponse + '</span></p>';
+        $(".botText:last").replaceWith(botHtml);
+      }
+    }, 100);
+  
+    setInterval(function () {
+      document.getElementById("chat-bar-bottom").scrollIntoView(true);
+    }, 10);
+  }
+  
 
 function getResponse() {
-    let userText = $("#textInput").val();
+  let userText = $("#textInput").val();
 
-    if (userText == "") {
-return;
-}
-else {
-// Run the rest of the code
-    
-}
+  if (userText == "") {
+    return;
+  } else {
+    // Run the rest of the code
+  }
 
-    let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
+  let userHtml = '<p class="userText"><span>' + userText + '</span></p>';
 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
-    setTimeout(() => {
-        getHardResponse(userText);
-    }, 1000)
-
+  setTimeout(() => {
+    getHardResponse(userText);
+  }, 1000);
 }
 
 function buttonSendText(sampleText) {
-    let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
+  let userHtml = '<p class="userText"><span>' + sampleText + '</span></p>';
 
-    $("#textInput").val("");
-    $("#chatbox").append(userHtml);
-    document.getElementById("chat-bar-bottom").scrollIntoView(true);
-
+  $("#textInput").val("");
+  $("#chatbox").append(userHtml);
+  document.getElementById("chat-bar-bottom").scrollIntoView(true);
 }
 
 function sendButton() {
-    getResponse();
+  getResponse();
 }
 
-
 $("#textInput").keypress(function (e) {
-    if (e.which == 13) {
-        getResponse();
-    }
-}); 
+  if (e.which == 13) {
+    getResponse();
+  }
+});
+
 
