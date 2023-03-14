994d614cee107c0377694c429f3a9e96599f5e39
nderimajdari <123307571+nderimajdari@users.noreply.github.com>
2023-03-14 17:17:42 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,17 +1,15 @@
-var coll = document.getElementsByClassName("collapsible");
+const coll = document.getElementsByClassName("collapsible");
 
-for (let i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function () {
-    this.classList.toggle("active");
-
-    var content = this.nextElementSibling;
-
-    if (content.style.maxHeight) {
-      content.style.maxHeight = null;
-    } else {
-      content.style.maxHeight = content.scrollHeight + "px";
-    }
-  });
+function addCollapsibleEventListeners() {
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", () => {
+      coll[i].classList.toggle("active");
+      const content = coll[i].nextElementSibling;
+      content.style.maxHeight
+        ? (content.style.maxHeight = null)
+        : (content.style.maxHeight = content.scrollHeight + "px");
+    });
+  }
 }
 
 function getTime() {
