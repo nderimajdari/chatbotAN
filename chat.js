4b2223798a5faf9664fb150c6dcf15cd76c6a147
nderimajdari <123307571+nderimajdari@users.noreply.github.com>
2023-03-25 23:03:49 +0100

    Update chat.js

--- chat.js
+++ chat.js
@@ -1,31 +1,23 @@
 const coll = document.getElementsByClassName("collapsible");
 
-function addCollapsibleEventListeners() {
-  for (let i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", () => {
-      coll[i].classList.toggle("active");
-      const content = coll[i].nextElementSibling;
-      content.style.maxHeight
-        ? (content.style.maxHeight = null)
-        : (content.style.maxHeight = content.scrollHeight + "px");
+const addCollapsibleEventListeners = () => {
+  for (const item of coll) {
+    item.addEventListener("click", () => {
+      item.classList.toggle("active");
+      const content = item.nextElementSibling;
+      content.style.maxHeight = content.style.maxHeight ? null : `${content.scrollHeight}px`;
     });
   }
-}
+};
 
-function getTime() {
+const getTime = () => {
   const today = new Date();
-  let hours = today.getHours();
-  let minutes = today.getMinutes();
-
-  hours = hours < 10 ? "0" + hours : hours;
-  minutes = minutes < 10 ? "0" + minutes : minutes;
-
-  const time = hours + ":" + minutes;
+  const time = today.toLocaleTimeString();
   return time;
-}
+};
 
 let firstMessage = "Hello, I'm chatbot AN! How can I help you today?";
-document.getElementById("botStarterMessage").innerHTML = '<p class="botText"><span>' + firstMessage + '</span></p>';
+document.getElementById("botStarterMessage").innerHTML = `<p class="botText"><span>${firstMessage}</span></p>`;
 
 let time = getTime();
 $("#chat-timestamp").append(time);
@@ -36,92 +28,85 @@ let isAnsweringQuestion = false;
 let intervalId = null;
 let scrollIntervalId = null;
 
-function getHardResponse(userText) {
+const getHardResponse = (userText) => {
   let botResponse = getBotResponse(userText);
-  let botHtml = '<p class="botText"><span>|</span></p>';
+  let botHtml = `<p class="botText"><span>|</span></p>`;
 
-  // append bot's response after the last user message
   $(".userText:last").after(botHtml);
 
   let i = 0;
-  intervalId = setInterval(function () {
-    if (i % 2 == 0) {
-      $(".botText:last span").text(botResponse.slice(0, i) + "_");
-    } else {
-      $(".botText:last span").text(botResponse.slice(0, i));
-    }
+  intervalId = setInterval(() => {
+    $(".botText:last span").text(`${botResponse.slice(0, i)}${i % 2 === 0 ? "_" : ""}`);
     i++;
 
     if (i > botResponse.length) {
       clearInterval(intervalId);
 
-      // fade out cursor after 2 seconds
-      setTimeout(function() {
+      setTimeout(() => {
         $(".botText:last span:not(:last-child)").fadeOut("slow");
       }, 2000);
 
-      let botHtml = '<p class="botText"><span>' + botResponse + "</span></p>";
+      let botHtml = `<p class="botText"><span>${botResponse}</span></p>`;
       $(".botText:last").replaceWith(botHtml);
-      
+
       isAnsweringQuestion = false;
       processQuestionQueue();
-      clearInterval(scrollIntervalId); // clear the scrolling interval
+      clearInterval(scrollIntervalId);
     }
   }, 150);
 
-  scrollIntervalId = setInterval(function () {
+  scrollIntervalId = setInterval(() => {
     document.getElementById("chat-bar-bottom").scrollIntoView(true);
   }, 10);
-}
+};
 
-async function queueQuestion(userText) {
+const queueQuestion = async (userText) => {
   questionQueue.push(userText);
   await processQuestionQueue();
-}
+};
 
-async function processQuestionQueue() {
+const processQuestionQueue = async () => {
   if (questionQueue.length > 0 && !isAnsweringQuestion) {
     isAnsweringQuestion = true;
     const question = questionQueue.pop();
     clearInterval(intervalId);
-    clearInterval(scrollIntervalId); // clear the scrolling interval
+    clearInterval(scrollIntervalId);
     await getHardResponse(question);
     await processQuestionQueue();
     isAnsweringQuestion = false;
   }
-}
+};
 
-function getResponse() {
+const getResponse = () => {
   let userText = $("#textInput").val();
 
-  if (userText.trim() == "") {
+  if (userText.trim() === "") {
     return;
-  } else {
   }
 
-  let userHtml = '<p class="userText"><span>' + userText + "</span></p>";
+  let userHtml = `<p class="userText"><span>${userText}</span></p>`;
 
   $("#textInput").val("");
   $("#chatbox").append(userHtml);
   document.getElementById("chat-bar-bottom").scrollIntoView(true);
 
   queueQuestion(userText);
-}
+};
 
-function buttonSendText(sampleText) {
-  let userHtml = '<p class="userText"><span>' + sampleText + "</span></p>";
+const buttonSendText = (sampleText) => {
+  let userHtml = `<p class="userText"><span>${sampleText}</span></p>`;
 
   $("#textInput").val("");
   $("#chatbox").append(userHtml);
   document.getElementById("chat-bar-bottom").scrollIntoView(true);
-}
+};
 
-function sendButton() {
+const sendButton = () => {
   getResponse();
-}
+};
 
-$("#textInput").keypress(function (e) {
-  if (e.which == 13) {
+$("#textInput").keypress((e) => {
+  if (e.which === 13) {
     getResponse();
   }
 });
